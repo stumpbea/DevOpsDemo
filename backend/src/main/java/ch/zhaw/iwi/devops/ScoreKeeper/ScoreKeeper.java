fbe300334343f2ae53ea9f71911a0fb332fdf5a4
stumpbea <stumpbea@students.zhaw.ch>
2024-04-06 17:27:07 +0200

    Grundstruktur ScoreKeeper.java

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/ScoreKeeper/ScoreKeeper.java
@@ -0,0 +1,31 @@
+package ch.zhaw.iwi.devops.ScoreKeeper;
+
+public class ScoreKeeper {
+    int a = 0;
+    int b = 0;
+    String A = "Stump";
+    String B = "Moser";
+
+
+    void scoreTeamA1(){
+
+    }
+    void scoreTeamA2(){
+
+    }
+    void scoreTeamA3(){
+
+    }
+    void scoreTeamB1(){
+
+    }
+    void scoreTeamB2(){
+
+    }
+    void scoreTeamB3(){
+
+    }
+    String getScore(){
+        return A + " " + String.valueOf(a) + ":" + String.valueOf(b) + " " + B;
+    }   
+}
