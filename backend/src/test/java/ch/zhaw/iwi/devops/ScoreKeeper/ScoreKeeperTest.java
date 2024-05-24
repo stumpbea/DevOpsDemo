91c45c70bcc09c7c573fa0a775398cf33a243f58
stumpbea <stumpbea@students.zhaw.ch>
2024-05-24 15:00:04 +0200

    Fixed ScoreKeeper Tests

--- backend/src/test/java/ch/zhaw/iwi/devops/ScoreKeeper/ScoreKeeperTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/ScoreKeeper/ScoreKeeperTest.java
@@ -103,12 +103,10 @@ class ScoreKeeperTest {
     }
     @Test
     void ScoreKeeper_Moser1_Stump1() {
-
         ScoreKeeper Score = new ScoreKeeper();
         Score.scoreTeamA2();
-        Score.scoreTeamB2();
+        Score.scoreTeamB1();
         Score.reduceScoreTeamA1();
-        Score.reduceScoreTeamB1();
         Assertions.assertEquals("Stump 001:001 Moser", Score.getScore());
     }
 }
